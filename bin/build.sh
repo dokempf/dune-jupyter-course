cb07af57cc65c75214784efe22c06eabfa61c4a0
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-03-23 15:30:54 +0100

    Patch out std::call_once to complete the first exercise

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Patch dune-geometry
+pushd dune/dune-geometry
+if [ ! -f "remove_std_call_once.patch" ]; then
+    cp ../../patches/remove_std_call_once.patch .
+    git apply remove_std_call_once.patch
+fi
+popd
+
+# Build using dunecontrol
 ./dune/dune-common/bin/dunecontrol --opts=./dune/config.opts --module=jupyter-kernel --builddir=$(pwd)/build all
 ./dune/dune-common/bin/dunecontrol --only=jupyter-kernel --builddir=$(pwd)/build bexec make install_kernelspec
--- dune/jupyter-kernel/dune/jupyter.hh
+++ dune/jupyter-kernel/dune/jupyter.hh
@@ -1,10 +1,13 @@
 #ifndef DUNE_JUPYTER_HH
 #define DUNE_JUPYTER_HH
 
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
 // Include all headers of all Dune modules
 #include<dune/common.hh>
 #include<dune/geometry.hh>
 #include<dune/grid.hh>
+#include<dune/pdelab.hh>
 
 // Include jupyter extension headers
 #include<dune/jupyter-kernel/printgrid.hh>
