4df4d1add8cce237f6d6dbbe3b52d3d4d9b206f4
Alexandra Datz <datz@stud.uni-heidelberg.de>
2021-07-12 14:53:24 +0200

    changed dimension check for grids

--- dune/jupyter-kernel/dune/jupyter-kernel/printgrid.hh
+++ dune/jupyter-kernel/dune/jupyter-kernel/printgrid.hh
@@ -15,99 +15,30 @@ namespace Dune {
   {
     //check for grid dimension
     const int dim = Grid::dimension;
-    if(dim > 2){
+    if constexpr (dim == 2){
+      // Write out the PNG file using Dune functionality
+      int argc;
+      char** argv;
+      const auto& helper = Dune::MPIHelper::instance(argc, argv);
+      Dune::printGrid(*grid, helper, "gridexercise");
+
+      // Read it back into a string buffer
+      std::ifstream fin("gridexercise_0.png", std::ios::binary);
+      std::stringstream buffer;
+      buffer << fin.rdbuf();
+      fin.close();
+
+      // Return a JSON object for Jupyter
       auto bundle = nlohmann::json::object();
-      bundle["text/plain"] = "Visualization is only available for 2D grids.";
+      bundle["image/png"] = xtl::base64encode(buffer.str());
       return bundle;
     }
-    // Write out the PNG file using Dune functionality
-    int argc;
-    char** argv;
-    const auto& helper = Dune::MPIHelper::instance(argc, argv);
-    Dune::printGrid(*grid, helper, "gridexercise");
 
-    // Read it back into a string buffer
-    std::ifstream fin("gridexercise_0.png", std::ios::binary);
-    std::stringstream buffer;
-    buffer << fin.rdbuf();
-    fin.close();
-
-    // Return a JSON object for Jupyter
     auto bundle = nlohmann::json::object();
-    bundle["image/png"] = xtl::base64encode(buffer.str());
+    bundle["text/plain"] = "Visualization is only available for 2D grids.";
     return bundle;
+
   }
-
-/*
-  template<int dim, typename T, typename U, template<int,typename> typename Grid>
-  nlohmann::json mime_bundle_repr(const std::unique_ptr<Grid<dim,T>, U > & grid)
-  {
-    // Write out the PNG file using Dune functionality
-    int argc;
-    char** argv;
-    const auto& helper = Dune::MPIHelper::instance(argc, argv);
-    Dune::printGrid(*grid, helper, "gridexercise");
-
-    // Read it back into a string buffer
-    std::ifstream fin("gridexercise_0.png", std::ios::binary);
-    std::stringstream buffer;
-    buffer << fin.rdbuf();
-    fin.close();
-
-    // Return a JSON object for Jupyter
-    auto bundle = nlohmann::json::object();
-    bundle["image/png"] = xtl::base64encode(buffer.str());
-    return bundle;
-  }
-
-  //if grid has dim=3 it cannot be printed
-  template<typename T, typename U, template<int,typename> typename Grid>
-  nlohmann::json mime_bundle_repr(const std::unique_ptr<Grid<3,T>, U > & grid)
-  {
-      auto bundle = nlohmann::json::object();
-      bundle["text/html"] = "Visualization is only available for 2D grids.";
-      return bundle;
-  }
-*/
-
-  //the function grid() of a gridview returns a reference to a grid, but this should be done using concepts
-  /*template<int dim, typename T, template<int,typename> typename Grid>
-  nlohmann::json mime_bundle_repr(const Grid<dim,T>& grid)
-  {
-    // Write out the PNG file using Dune functionality
-    int argc;
-    char** argv;
-    const auto& helper = Dune::MPIHelper::instance(argc, argv);
-    Dune::printGrid(grid, helper, "gridexercise");
-
-    // Read it back into a string buffer
-    std::ifstream fin("gridexercise_0.png", std::ios::binary);
-    std::stringstream buffer;
-    buffer << fin.rdbuf();
-    fin.close();
-
-    // Return a JSON object for Jupyter
-    auto bundle = nlohmann::json::object();
-    bundle["image/png"] = xtl::base64encode(buffer.str());
-    return bundle;
-  }
-
-  template<typename T, template<int,typename> typename Grid>
-  nlohmann::json mime_bundle_repr(const Grid<3,T>& grid)
-  {
-      auto bundle = nlohmann::json::object();
-      bundle["text/html"] = "Visualization is only available for 2D grids.";
-      return bundle;
-  }
-
-  //printgrid function for grid
-  template <typename Grid>
-  nlohmann::json mime_bundle_repr(const GridView<Grid> & gridview)
-  {
-      const auto& grid = gridview.grid();
-      return mime_bundle_repr(grid);
-  }
-  */
 }
 
 #endif // DUNE_JUPYTER_KERNEL_PRINTGRID_HH
