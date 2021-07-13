f18617c9ba481e0a43c455580c76ec98ac492119
Alexandra Datz <datz@stud.uni-heidelberg.de>
2021-07-13 12:18:53 +0200

    added subsamplinVTKwriter

--- dune/jupyter-kernel/dune/jupyter-kernel/vtkwriter.hh
+++ dune/jupyter-kernel/dune/jupyter-kernel/vtkwriter.hh
@@ -17,6 +17,17 @@ namespace Dune {
     return bundle;
   }
 
+  template<typename GV>
+  nlohmann::json mime_bundle_repr(Dune::SubsamplingVTKWriter<GV>& vtkwriter)
+  {
+    // Write out the VTK file into a temporary file
+    vtkwriter.write("output", Dune::VTK::appendedraw);
+    auto bundle = nlohmann::json::object();
+    bundle["text/html"] = "VTK Output has been written to file. You can <a href=\"output.vtu\">download it</a> and visualize it in ParaView.";
+    return bundle;
+  }
+
+
 } // namespace Dune
 
 #endif
