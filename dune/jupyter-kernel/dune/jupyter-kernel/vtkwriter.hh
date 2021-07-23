a6c7070889d15a1ec52f97e12f5e75569cf87560
Alexandra Datz <datz@stud.uni-heidelberg.de>
2021-07-23 13:32:44 +0200

    added option to specify name of output file

--- dune/jupyter-kernel/dune/jupyter-kernel/vtkwriter.hh
+++ dune/jupyter-kernel/dune/jupyter-kernel/vtkwriter.hh
@@ -10,23 +10,46 @@ namespace Dune {
   template<typename GV>
   nlohmann::json mime_bundle_repr(Dune::VTKWriter<GV>& vtkwriter)
   {
+    //read name from temporary file
+    std::string outputname;
+    std::ifstream myfile("name.txt");
+    if (myfile.is_open())
+    {
+      std::getline(myfile,outputname);
+      myfile.close();
+    }
+    else
+      outputname = "output";
+
     // Write out the VTK file into a temporary file
-    vtkwriter.write("output", Dune::VTK::appendedraw);
+    vtkwriter.write(outputname, Dune::VTK::appendedraw);
     auto bundle = nlohmann::json::object();
-    bundle["text/html"] = "VTK Output has been written to file. You can <a href=\"output.vtu\">download it</a> and visualize it in ParaView.";
+    std::string bundletxt = "VTK Output has been written to file. You can <a href=\""+ outputname + ".vtu\">download it</a> and visualize it in ParaView.";
+    bundle["text/html"] = bundletxt;
     return bundle;
   }
 
   template<typename GV>
   nlohmann::json mime_bundle_repr(Dune::SubsamplingVTKWriter<GV>& vtkwriter)
   {
+    //read name from temporary file
+    std::string outputname;
+    std::ifstream myfile("name.txt");
+    if (myfile.is_open())
+    {
+      std::getline(myfile,outputname);
+      myfile.close();
+    }
+    else
+      outputname = "output";
+
     // Write out the VTK file into a temporary file
-    vtkwriter.write("output", Dune::VTK::appendedraw);
+    vtkwriter.write(outputname, Dune::VTK::appendedraw);
     auto bundle = nlohmann::json::object();
-    bundle["text/html"] = "VTK Output has been written to file. You can <a href=\"output.vtu\">download it</a> and visualize it in ParaView.";
+    std::string bundletxt = "VTK Output has been written to file. You can <a href=\""+ outputname + ".vtu\">download it</a> and visualize it in ParaView.";
+    bundle["text/html"] = bundletxt;
     return bundle;
   }
-
 
 } // namespace Dune
 
